c2ad4e0f00f048e1cd0b2d2157f6b5d3959a028b
Simon Beaulieu <sbeaulieu.stage@sodifrance.fr>
2019-02-26 09:29:28 +0100

    pizza instance as tab

--- PizzaHashCode/Program.cs
+++ PizzaHashCode/Program.cs
@@ -8,8 +8,46 @@ namespace PizzaHashCode
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+            string inputFile = "3 5 1 6\nTTTTT\nTMMMT\nTTTTT";
+            
+
+            //stockage des données dans les différents variables
+            string[] lines = inputFile.Split('\n');
+            string[] vars = lines[0].Split(' ');
+
+            string one = vars[0];
+            int rows = int.Parse(one);
+
+            string two = vars[1];
+            int columns = int.Parse(two);
+
+            string three = vars[2];
+            int minIngr = int.Parse(three);
+
+            string four = vars[3];
+            int maxCells = int.Parse(four);
+
+            //boucles de mise en place de la pizza dans un tableau
+            char[,] pizza = new char[rows, columns];
+            int Mnb=0, Tnb=0;
+            for (int i = 0; i <rows ; i++)
+            {
+                var ligne = lines[i + 1].ToCharArray();
+                for (int j = 0; j <columns ; j++)
+                {
+                    if (ligne[j] == 'T') Tnb++;
+                    else Mnb++;
+                    pizza[i, j] = ligne[j];
+                }
+            }
+            Console.Write("Tomatoes: "+ Tnb.ToString() +" Mushrooms: "+ Mnb);
+
+        
+
+            Console.ReadKey();
         }
     }
 }
